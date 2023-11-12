43196a91820efdf0dd581684fd02a6a7b5614efe
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-11-12 20:52:53 +0200

    update page & layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,25 @@
 import "@styles/global.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
             {children}
-        </main>
+          </main>
+        </Provider>
       </body>
     </html>
   );
